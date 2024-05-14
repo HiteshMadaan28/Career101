2505796f869bf5c8f5f2a1dfdd1ee6ac83506859
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-05-14 23:38:17 +0530

    Initial Commit

---
+++ Career101/Career101App.swift
@@ -0,0 +1,17 @@
+//
+//  Career101App.swift
+//  Career101
+//
+//  Created by Hitesh Madaan on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Career101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
